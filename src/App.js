42cbba9621cc61a25847370c9d96685837e8be91
SaraL6 <71009666+SaraL6@users.noreply.github.com>
2020-12-22 19:59:22 +0100

    Checkout v1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import React,{ useEffect } from 'react';
-import './App.css';
-import Header from './Header';
-import Home from './Home';
+import React, { useEffect } from "react";
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
-
   // fetch api here
-  useEffect(() => 
-  {
-    
-    fetch('http://localhost:8000/api/test')
-    .then(function(response){
-      response.json().then(function(resp){
+  useEffect(() => {
+    fetch("http://localhost:8000/api/test").then(function (response) {
+      response.json().then(function (resp) {
         console.log(resp);
-      })
-    })
+      });
+    });
   });
   return (
     // BEM
-    <div className="App">
-     <Header />
-     <Home />
-    </div>
+    <Router>
+      <div className="App">    
+          <Header />
+
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
