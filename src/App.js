d63308288e118364d76b4fd617dec69e5e586450
SaraL6 <71009666+SaraL6@users.noreply.github.com>
2021-01-02 20:05:33 +0100

    Login

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Checkout from "./Checkout";
+import Login from "./Login";
 
 function App() {
   // fetch api here
@@ -17,14 +18,22 @@ function App() {
   return (
     // BEM
     <Router>
-      <div className="App">    
-          <Header />
+      <div className="App">
 
         <Switch>
+          <Route path="/login">
+            <Login/>
+          </Route>
+
           <Route path="/checkout">
+          <Header />
+
             <Checkout />
           </Route>
+
           <Route path="/">
+          <Header />
+
             <Home />
           </Route>
         </Switch>
--- src/Header.js
+++ src/Header.js
@@ -23,10 +23,13 @@ function Header() {
       </div>
 
       <div className="header__nav">
+        <Link to='/login'>
         <div className="header__option">
           <span className="header__optionLineOne">Hello Guest</span>
           <span className="header__optionLineTwo">Sign In</span>
         </div>
+        </Link>
+
 
         <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
@@ -37,6 +40,7 @@ function Header() {
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
         </div>
+
         <Link to="/checkout">
           <div className="header__optionBasket">
             <ShoppingBasketIcon />
